c1265d599debaef66992c92af3ddc397cfc8aa46
Yuwen Li <muzlee1113@gmail.com>
2019-03-13 09:56:02 -0500

    login signup btn

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -31,18 +31,31 @@ var messages = {
 
 
 $(document).ready(function () {
-    //clickhandlers for log in model
+    //clickhandlers for sign up button model
+    $("#signupBtn").on("click", function (e) {
+        e.preventDefault()
+        //POST: new user
+        //data structure
+        // {
+        //     name: name,
+        //     password: password,
+        // }
 
-    //clickhandlers for sign up model
-    //POST: new user
-    //data structure
-    // {
-    //     name: name,
-    //     password: password,
-    // }
+
+    })
+
+    //clickhandlers for login button model
+    $("#loginBtn").on("click", function (e) {
+        e.preventDefault()
+
+
+
+
+    })
+
 
     //clickhandler for showing the create pet modal
-    $("#createPet").on("click",function(e){
+    $("#createPet").on("click", function (e) {
         e.preventDefault()
         // run the function that displays pet options
         creatNewPetList()
@@ -54,7 +67,7 @@ $(document).ready(function () {
     })
 
     //clickhandler for select a pet
-    $("#createPetModal").on("click",".selectPetBox",function(e){
+    $("#createPetModal").on("click", ".selectPetBox", function (e) {
         e.preventDefault()
         console.log("click!")
         // change the border color and thickness of the selected box
@@ -66,7 +79,7 @@ $(document).ready(function () {
     })
 
     //clickhandler for submit/create a pet
-    $("#createNewPetBtn").on("click", function(e){
+    $("#createNewPetBtn").on("click", function (e) {
         e.preventDefault()
         let name = $("#newPetName").val()
         let img = selectedPetSrc
@@ -81,19 +94,19 @@ $(document).ready(function () {
             url: "/api/newpet",
             type: 'POST',
             data: requestBody,
-        }).then(function(result){
+        }).then(function (result) {
             console.log("New Pet has been created")
             location.reload()
         })
     })
-    
+
 
     // a function that displays pet options
-    const creatNewPetList = function(){
+    const creatNewPetList = function () {
         // run the src of gifs in a for loop
-        for(let i = 0; i<19; i++){
+        for (let i = 0; i < 19; i++) {
             let src = "/assets/img/gif/pet" + i + ".gif"
-            let articleDiv = '<div class="selectPetBox p-2" style="align-items: center" data-img="'+ src +'"><div style="align-self: center; justify-self: center;"><img src="' + src +'"></div></div>'
+            let articleDiv = '<div class="selectPetBox p-2" style="align-items: center" data-img="' + src + '"><div style="align-self: center; justify-self: center;"><img src="' + src + '"></div></div>'
             // append to div cards-createNewPet
             $(".cards-createNewPet").append(articleDiv)
         }
@@ -158,7 +171,7 @@ const showPetInfo = function (id) {
         $("#sleepyBar").attr("style", "width:" + sleepy + "%")
         $("#happyBar").attr("style", "width:" + happy + "%")
         // manage the message:
-        let message = messageGenerator(parseInt(data.hp),parseInt(data.hungry),parseInt(data.sleepy),parseInt(data.happy))
+        let message = messageGenerator(parseInt(data.hp), parseInt(data.hungry), parseInt(data.sleepy), parseInt(data.happy))
         console.log("The message is:")
         console.log(message)
         $("#message").html(message)
@@ -168,44 +181,44 @@ const showPetInfo = function (id) {
 }
 
 
-const messageGenerator = function(hp, hungry, sleepy, happy){
+const messageGenerator = function (hp, hungry, sleepy, happy) {
     var message = ''
-        if (hp === 1) {
-            message = message.concat(messages.hp1)
-        } else if (hp === 2) {
-            message = message.concat(messages.hp2)
-        }
-        if(hungry === 0){
-            message = message.concat(messages.hungry0)
-        }
-        if(hungry === 1){
-            message = message.concat(messages.hungry1)
-        }
-        if(hungry === 2){
-            message = message.concat(messages.hungry2)
-        }
-        if(sleepy === 0){
-            message = message.concat(messages.sleepy0)
-        }
-        if(sleepy === 1){
-            message = message.concat(messages.sleepy1)
-        }
-        if(sleepy === 2){
-            message = message.concat(messages.sleepy2)
-        }
-        if(happy === 0){
-            message = message.concat(messages.happy0)
-        }
-        if(happy === 1){
-            message = message.concat(messages.happy1)
-        }
-        if(happy === 2){
-            message = message.concat(messages.happy2)
-        }
-        if(happy > 2 && sleepy > 2 && hungry >2){
-            let random = Math.floor(Math.random()*messages.goodStatus.length)
-            message = message.concat(messages.goodStatus[random])
-        }
+    if (hp === 1) {
+        message = message.concat(messages.hp1)
+    } else if (hp === 2) {
+        message = message.concat(messages.hp2)
+    }
+    if (hungry === 0) {
+        message = message.concat(messages.hungry0)
+    }
+    if (hungry === 1) {
+        message = message.concat(messages.hungry1)
+    }
+    if (hungry === 2) {
+        message = message.concat(messages.hungry2)
+    }
+    if (sleepy === 0) {
+        message = message.concat(messages.sleepy0)
+    }
+    if (sleepy === 1) {
+        message = message.concat(messages.sleepy1)
+    }
+    if (sleepy === 2) {
+        message = message.concat(messages.sleepy2)
+    }
+    if (happy === 0) {
+        message = message.concat(messages.happy0)
+    }
+    if (happy === 1) {
+        message = message.concat(messages.happy1)
+    }
+    if (happy === 2) {
+        message = message.concat(messages.happy2)
+    }
+    if (happy > 2 && sleepy > 2 && hungry > 2) {
+        let random = Math.floor(Math.random() * messages.goodStatus.length)
+        message = message.concat(messages.goodStatus[random])
+    }
     return message
 }
 
